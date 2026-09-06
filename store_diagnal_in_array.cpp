0fb6a674606d9931dcb044aa1168abc8c1272f35
kirti0924 <kirti9592866701@gmail.com>
2026-09-07 00:47:57 +0530

    Create store_diagnal_in_array.cpp

---
+++ store_diagnal_in_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    int b[3];
+    int i,j,k=0;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i==j)
+            {
+                b[k]=a[i][j];
+                k++;
+            }
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        cout<<b[i]<<" ";
+    }
+    return 0;
+}

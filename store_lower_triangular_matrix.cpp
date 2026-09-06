3af392e39188fcbaa38b7d7437e70eb293adf07f
kirti0924 <kirti9592866701@gmail.com>
2026-09-07 00:56:41 +0530

    Create store_lower_triangular_matrix.cpp

---
+++ store_lower_triangular_matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    int b[3];
+    int i,j,k=0;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+               b[k]=a[i][j];
+                k++;
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        cout<<b[i]<<" ";
+    }
+    return 0;
+}
